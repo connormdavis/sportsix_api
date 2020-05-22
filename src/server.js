d5a1a6dd18b9eb10524ead4f7e72360b991c4aae
Connor Davis <condavis6@gmail.com>
2020-05-22 13:10:01 -0400

    added CORS

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ var express = require('express');        // call express
 var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
 import morgan from 'morgan';
+import cors from 'cors';
 import v1Routes from './routes/v1';
 
 // set up parsing body of requests
@@ -10,6 +11,9 @@ app.use(bodyParser.json());
 
 // enable/disable http request logging
 app.use(morgan('dev'));
+
+// enable/disable cross origin resource sharing if necessary
+app.use(cors());
 
 var port = process.env.PORT || 8080;        // set our port
 
